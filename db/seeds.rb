0e8ef4a4df66580c797e1544249221eea817f02a
unknown <valmocinajayangelo@gmail.com>
2019-09-01 23:57:33 +0800

    First commit

---
+++ db/seeds.rb
@@ -0,0 +1,8 @@
+
+
+    Post.create([
+        { title: "Monday", body: "Starting my day again"},
+        { title: "Tuesday", body: "Everything is awesome"},
+        { title: "Wendesday", body: "Life is greater when you share it"},
+        { title: "Thursday", body: "No hate, spread love."}
+    ])
